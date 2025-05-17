74dfea2b6ea08d22ee54ff52be134c265dbf8721
MuhAminSyam <aminsyammuh@gmail.com>
2025-05-17 15:05:29 +0700

    akses modifier (public) dan int main

--- Activity8.cpp
+++ Activity8.cpp
@@ -204,3 +204,23 @@ using namespace std; // Menggunakan namespace std
 class buku // Deklarasi class buku
 {
     string judul;
+
+public: // Bagian public
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this;
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
+
+int main() // Fungsi utama program
+{
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul();
+    return 0;
+}
